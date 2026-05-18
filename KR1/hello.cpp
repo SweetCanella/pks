6e73e2eda4198c66a6a391c93d2524898a279c3e
Gesha <baramasya@gmail.com>
2026-05-18 11:42:29 +0300

    Add KR1 Hello World project

---
+++ KR1/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+#include <iostream>
+
+void printHelloWorld(std::ostream& os) {
+    os << "Hello, World!\n";
+}
---
+++ KR1/hello.h
@@ -0,0 +1,3 @@
+#include <iosfwd>
+
+void printHelloWorld(std::ostream&);
